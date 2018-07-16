6a3b0c9287da8901f24c35b6774b74aed2d06cd8
Clement Francois <clement.francois@epitech.eu>
2018-07-16 16:37:54 +0200

    tflearn_bootstrap: ADD: tflearn bootstrap driver

---
+++ tflearn_bootstrap.py
@@ -0,0 +1,29 @@
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import tflearn.datasets.mnist as mnist
+
+training_images, training_labels, test_training_images, test_training_labels = mnist.load_data(one_hot=True)
+
+training_images = training_images.reshape([-1, 28, 28, 1])
+test_training_images = test_training_images.reshape([-1, 28, 28, 1])
+
+convnet = input_data(shape=[None, 28, 28, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 10, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=0.01, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet)
+model.fit({'input': training_images}, {'targets': training_images}, n_epoch=10, validation_set=({'input': test_training_images}, {'targets': test_training_images}), 
+    snapshot_step=500, show_metric=True, run_id='mnist')
+model.save(model_file='test.model')
